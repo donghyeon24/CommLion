eb1d54c00b7b8a772edfc3966e5f2d4c7fb2031f
div-ein <esatar658@gmail.com>
2021-06-30 15:43:14 +0900

    url연결

---
+++ README.md
@@ -0,0 +1,32 @@
+## DONGHYEON branch
+
+## 🌍 CommLion이란?
+
+- 작성해주세요
+  <br>
+
+## 📌 주요 기능
+
+- **작성해주세요** : 작성해주세요
+  <br>
+
+## 📆 개발 일정
+
+1. 기획
+2. 디자인
+3. 프론트엔드
+4. 백엔드
+   <br>
+
+## 😀 함께한 사람들
+
+- 경상대학교 멋쟁이사자처럼 9기 성규동
+- 경상대학교 멋쟁이사자처럼 8기 김동현
+- 경상대학교 멋쟁이사자처럼 8기 박지은
+- 경상대학교 멋쟁이사자처럼 9기 양소열
+  <br>
+
+## 📗 참고 자료
+
+- Notion : https://www.notion.so/map-mo-3ae5a4750c70464cb00ee2719c9adae5
+- Figma : https://www.figma.com/file/xw27vs6gO3Z3RplGw7XtQe/map-mo?node-id=95%3A37
---
+++ config/urls.py
@@ -0,0 +1,32 @@
+"""config URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from commlion import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
+    path('notice/', views.notice, name='notice'),
+    path('session/', views.session, name='session'),
+    path('qna/main/', views.qnaMain, name='qnaMain'),
+    path('qna/detail/', views.qnaDetail, name='qnaDetail'),
+    path('qna/write/', views.qnaWrite, name='qnaWrite'),
+    path('project/main/', views.projectMain, name='projectMain'),
+    path('project/detail/', views.projectDetail, name='projectDetail'),
+    path('project/write/', views.projectWrite, name='projectWrite'),
+]
