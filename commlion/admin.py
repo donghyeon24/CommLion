3a7cb4a33a0e22ec3d6afef814fd548b3076642a
박지은 <jieun1172@naver.com>
2021-06-30 23:48:28 +0900

    w

--- commlion/admin.py
+++ commlion/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Uni,Student,NoticePost,SessionPost,QnaPost,Comment,ProjectPost
 
 # Register your models here.
+
+class UniAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Uni,UniAdmin)
+
+class StudentAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Student,StudentAdmin)
+
+class NoticePostAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(NoticePost,NoticePostAdmin)
+
+class SessionPostAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(SessionPost,SessionPostAdmin)
+
+class QnaPostAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(QnaPost,QnaPostAdmin)
+
+class CommentAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Comment,CommentAdmin)
+
+class ProjectPostAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(ProjectPost,ProjectPostAdmin)
+
+
