55fb4c64f17d345218752a44fa834aba165d60e3
Soyuly <thduf0623@likelion.org>
2021-07-01 21:48:51 +0900

    2nd edit

--- commlion/views.py
+++ commlion/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.utils import timezone
 from datetime import datetime, timezone
 from datetime import time
-from .models import NoticePost,SessionPost, Student
+from .models import NoticePost, SessionPost, Student
 
 # Create your views here.
 
@@ -15,14 +15,14 @@ def login(request):
     return render(request, 'login.html')
 
 
-def notice(request):
-    notices=NoticePost.objects.all()
-    return render(request, 'notice.html',{'notices':notices})
+def noticeMain(request):
+    notices = NoticePost.objects.all()
+    return render(request, 'notice-main.html', {'notices': notices})
 
 
-def session(request):
-    session=SessionPost.objects.get(id=1)
-    return render(request, 'session.html',{'session':session})
+def sessionMain(request):
+    session = SessionPost.objects.get(id=1)
+    return render(request, 'session-main.html', {'session': session})
 
 
 def qnaMain(request):
@@ -50,32 +50,31 @@ def projectWrite(request):
 
 
 def sessionWrite(request):
-  if request.method == 'POST':
-    sessionPost = SessionPost()
-    sessionPost.session_num = 0
-    sessionPost.session_year = 0
-    sessionPost.session_content = request.POST['content']
-    sessionPost.session_title = request.POST['title']
-    sessionPost.session_file = request.FILES['img']
-    sessionPost.student_id = Student.objects.get(student_id=0)
-    sessionPost.save()
+    if request.method == 'POST':
+        sessionPost = SessionPost()
+        sessionPost.session_num = 0
+        sessionPost.session_year = 0
+        sessionPost.session_content = request.POST['content']
+        sessionPost.session_title = request.POST['title']
+        sessionPost.session_file = request.FILES['img']
+        sessionPost.student_id = Student.objects.get(student_id=0)
+        sessionPost.save()
 
-    return redirect('session')
-  else:
-    return render(request, 'session-write.html')
+        return redirect('session')
+    else:
+        return render(request, 'session-write.html')
+
 
 def noticeWrite(request):
-  if request.method == 'POST':
-    noticePost = NoticePost()
-    noticePost.content = request.POST['content']
-    noticePost.title = request.POST['title']
-    noticePost.pub_date = timezone.datetime.now()
-    noticePost.student_id = Student.objects.get(student_id=0)
-    # 아이디값 변경
-    noticePost.save()
+    if request.method == 'POST':
+        noticePost = NoticePost()
+        noticePost.content = request.POST['content']
+        noticePost.title = request.POST['title']
+        noticePost.pub_date = timezone.datetime.now()
+        noticePost.student_id = Student.objects.get(student_id=0)
+        # 아이디값 변경
+        noticePost.save()
 
-    return redirect('notice')
-  else:
-    return render(request, 'notice-write.html')
-
-
+        return redirect('notice')
+    else:
+        return render(request, 'notice-write.html')
